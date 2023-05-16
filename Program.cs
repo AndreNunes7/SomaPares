48f987922cfef8fa72519b16a728e615c9b3c022
Andre <andrestanoga12@gmail.com>
2023-05-16 01:11:24 -0300

    First commit

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿namespace Array
+{
+    public static class Array
+    {
+        public static int Main(string[] args)
+        {
+            Console.WriteLine("-------------------------");
+          
+            int[] num = { 1, 4, 55, 33, 23, 24, 42, };
+
+            for (int i = 0; i < num.Length; i++)
+            {
+                if (num[i] % 2 == 0)
+                {
+
+                    Console.WriteLine("Os pares são: " + num[i]);
+
+                }
+                
+            }
+
+            Console.WriteLine("-------------------------");
+
+            Console.WriteLine("A soma dos pares é: " + somaPares(num));
+           
+            Console.WriteLine("-------------------------");
+            
+            return 0;
+        }
+
+        private static int somaPares(int[] num)
+        {
+            int soma = 0;
+
+            for (int i = 0; i < num.Length; i++)
+            {
+
+                if (num[i] % 2 == 0)
+                {
+                    soma += num[i];
+
+                }
+            }
+            return soma;
+        }
+     
+    }
+
+}
